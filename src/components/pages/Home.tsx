94e4fd442471d4aaad1037678a2c43b12a3c5440
okasyun <shunshun1207shun@keio.jp>
2022-09-19 20:31:26 +0900

    update contact

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,7 +3,6 @@ import Typography from "@mui/material/Typography";
 
 import "../../assets/home.css";
 import Header from "../template/Header";
-import Footer from "../template/Footer";
 
 const Home: FC = () => {
   return (
