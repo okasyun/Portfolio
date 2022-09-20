a1987c215cbdacc54fc7723bb2bc683ff84021db
okasyun <shunshun1207shun@keio.jp>
2022-09-21 06:42:23 +0900

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
-import { ThemeProvider } from "@mui/material";
 
-import { theme } from "./theme-option";
 import "./assets/reset.css";
 import "./assets/styled.css";
 
@@ -16,11 +14,9 @@ import "./assets/styled.css";
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </>
   );
 }
